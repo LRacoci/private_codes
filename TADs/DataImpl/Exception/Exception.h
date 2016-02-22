9824c996fd786a6b0ec7edd454352e1629582713
LRacoci <racoci.0@gmail.com>
2016-02-22 14:49:04 -0300

    Update of Exception

--- TADs/DataImpl/Exception/Exception.h
+++ TADs/DataImpl/Exception/Exception.h
@@ -5,11 +5,21 @@
 #ifndef EXCEPTION_DEFINED
 #define EXCEPTION_DEFINED
 
-#define try(code) \
 
+typedef struct sExcept{
+    bool isError;
+    String msg;
+} sException, *Excpetion;
 
-typedef struct sException{
-    String
-} sException, *Exception;
+#define Exception_empty(e) (!e)
+
+Exception newException(const String msg){
+    Exception new = malloc(sizeof(sException));
+    new->msg = String_copy(msg);
+    return new;
+}
+void freeException(Exception rmv){
+    if()
+}
 
 #endif
